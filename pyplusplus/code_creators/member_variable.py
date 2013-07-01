4dc3f6157546c6717d101f5a6c45232d9f921749
Christoph Koke <koke@kip.uni-heidelberg.de>
2013-07-01 20:23:34 +0200

    remove some redundancy from the member variable code generator
    Change-Id: I0ae01b22284f7f22265cf2bbdc40f185416b20ce

--- pyplusplus/code_creators/member_variable.py
+++ pyplusplus/code_creators/member_variable.py
@@ -47,56 +47,49 @@ class member_variable_t( member_variable_base_t ):
     def __init__(self, variable, wrapper=None ):
         member_variable_base_t.__init__( self, variable=variable, wrapper=wrapper )
 
+    def _create_property(self, name, getter, setter=None):
+        args = [name, getter]
+        if setter:
+            args.append(setter)
+
+        if self.declaration.type_qualifiers.has_static:
+            fn = 'add_static_property'
+        else:
+            fn = 'add_property'
+            if self.documentation:
+                args.append(self.documentation)
+        sep = self.indent(os.linesep + self.PARAM_SEPARATOR, 3)
+        return fn + '(' + sep.join(args) + ')'
+
+
     #>    On Wednesday, 19. April 2006 23:05, Ralf W. Grosse-Kunstleve wrote:
     #>   .add_property("p", make_function(&A::get_p, return_value_policy<reference_existing_object>()))
     def _generate_for_pointer( self ):
-        doc = '' #static property does not support documentation
-        if self.declaration.type_qualifiers.has_static:
-            add_property = 'add_static_property'
-        else:
-            if self.documentation:
-                doc = self.documentation
-            add_property = 'add_property'
-        answer = [ add_property ]
-        answer.append( '( ' )
-        answer.append('"%s"' % self.alias)
-        answer.append( self.PARAM_SEPARATOR )
+        name = '"%s"' % self.alias
 
         #according to David Abrahams:
         #http://mail.python.org/pipermail/c++-sig/2003-January/003276.html
         call_pol = call_policies.return_internal_reference().create( self )
         make_function = algorithm.create_identifier( self, '::boost::python::make_function' )
 
-        answer.append( '%(mk_func)s( (%(getter_type)s)(&%(wfname)s), %(call_pol)s )'
-                       % { 'mk_func' : make_function
-                           , 'getter_type' : self.wrapper.getter_type
+        getter = '%(mk_func)s( (%(getter_type)s)(&%(wfname)s), %(call_pol)s )' % {
+                             'mk_func' : make_function
+                           , 'getter_type' : self.wrapper.getter_type.decl_string
                            , 'wfname' : self.wrapper.getter_full_name
-                           , 'call_pol' : call_pol } )
-
+                           , 'call_pol' : call_pol }
+        setter = None
         #don't generate setter method, right now I don't know how to do it.
         if self.wrapper.has_setter:
-            answer.append( self.PARAM_SEPARATOR )
             call_pol = ''
             if not self.declaration.type_qualifiers.has_static:
                 call_pol = ", " + call_policies.with_custodian_and_ward_postcall( 1, 2 ).create(self)
-            answer.append( '%(mk_func)s( (%(setter_type)s)(&%(wfname)s)%(call_pol)s )'
-                       % { 'mk_func' : make_function
-                           , 'setter_type' : self.wrapper.setter_type
+            setter = '%(mk_func)s( (%(setter_type)s)(&%(wfname)s)%(call_pol)s )' % {
+                             'mk_func' : make_function
+                           , 'setter_type' : self.wrapper.setter_type.decl_string
                            , 'wfname' : self.wrapper.setter_full_name
-                           , 'call_pol' : call_pol } )
-        if doc:
-            answer.append( self.PARAM_SEPARATOR )
-            answer.append( doc )
-        answer.append( ' ) ' )
+                           , 'call_pol' : call_pol }
 
-        code = ''.join( answer )
-        if len( code ) <= self.LINE_LENGTH:
-            return code
-        else:
-            for i in range( len( answer ) ):
-                if answer[i] == self.PARAM_SEPARATOR:
-                    answer[i] = os.linesep + self.indent( self.indent( self.indent( answer[i] ) ) )
-            return ''.join( answer )
+        return self._create_property(name, getter, setter)
 
     def _generate_for_none_pointer( self ):
         tmpl = None
@@ -164,6 +157,57 @@ class member_variable_t( member_variable_base_t ):
         else:
             return self._generate_for_none_pointer()
 
+class mem_var_ref_t( member_variable_base_t ):
+    """
+    creates get/set accessors for class member variable, that has type reference.
+    """
+
+    def __init__(self, variable, wrapper ):
+        member_variable_base_t.__init__( self, variable=variable, wrapper=wrapper )
+        self.param_sep = os.linesep + self.indent( self.PARAM_SEPARATOR, 2 )
+        self.works_on_instance = False
+
+    def _create_def(self, args):
+        return "def(" + self.param_sep.join(args) + ")"
+
+    def _create_getter( self ):
+        name = '"get_%s"' % self.alias
+        function = '(%s)(&%s)' % ( self.wrapper.getter_type.decl_string,
+                self.wrapper.getter_full_name )
+        args = [name, function]
+        if (self.declaration.getter_call_policies
+                and not self.declaration.getter_call_policies.is_default()):
+            args.append(self.declaration.getter_call_policies.create( self ))
+        else:
+            args[-1] += os.linesep + self.indent( '/* undefined call policies */', 2 )
+        if self.documentation:
+            args.append(self.documentation)
+        return self._create_def(args)
+
+    def _create_setter( self ):
+        name = '"set_%s"' % self.alias
+        function = '(%s)(&%s)' % ( self.wrapper.setter_type.decl_string,
+                self.wrapper.setter_full_name )
+        args = [name, function]
+        if (self.declaration.setter_call_policies
+                and not self.declaration.setter_call_policies.is_default()):
+            args.append(self.declaration.getter_call_policies.create( self ))
+        else:
+            args[-1] += os.linesep + self.indent( '/* undefined call policies */', 2 )
+        if self.documentation:
+            args.append(self.documentation)
+        return self._create_def(args)
+
+    def _create_impl( self ):
+        #TODO: fix me, should not force class scope usage
+        answer = []
+        class_var_name = self.parent.class_var_name
+        answer.append( "%s.%s;" % (class_var_name, self._create_getter() ) )
+        if self.wrapper.has_setter:
+            answer.append( os.linesep )
+            answer.append( "%s.%s;" % (class_var_name, self._create_setter() ) )
+        return ''.join( answer )
+
 class member_variable_wrapper_t( code_creator.code_creator_t
                                  , declaration_based.declaration_based_t ):
     """
@@ -208,9 +252,9 @@ class member_variable_wrapper_t( code_creator.code_creator_t
         return self.parent.full_name + '::' + 'get_' + self.declaration.name
     getter_full_name = property( _get_getter_full_name )
 
-    def inst_arg_type( self, has_const ):
+    def _get_class_inst_type( self, add_const):
         inst_arg_type = declarations.declarated_t( self.declaration.parent )
-        if has_const:
+        if add_const:
             inst_arg_type = declarations.const_t(inst_arg_type)
         inst_arg_type = declarations.reference_t(inst_arg_type)
         return inst_arg_type
@@ -219,12 +263,11 @@ class member_variable_wrapper_t( code_creator.code_creator_t
         if self.declaration.type_qualifiers.has_static:
             arguments_types=[]
         else:
-            arguments_types=[ self.inst_arg_type(True) ]
+            arguments_types=[ self._get_class_inst_type(True) ]
 
-        return declarations.free_function_type_t.create_decl_string(
-                return_type=self.declaration.decl_type
-                , arguments_types=arguments_types
-                , with_defaults=False)
+        return declarations.free_function_type_t(
+                return_type=self._get_var_type()
+                , arguments_types=arguments_types)
     getter_type = property( _get_getter_type )
 
     def _get_setter_full_name(self):
@@ -235,43 +278,99 @@ class member_variable_wrapper_t( code_creator.code_creator_t
         if self.declaration.type_qualifiers.has_static:
             arguments_types=[ self.declaration.decl_type ]
         else:
-            arguments_types=[ self.inst_arg_type(False), self.declaration.decl_type  ]
+            arguments_types=[ self._get_class_inst_type(False),
+                    self.declaration.decl_type  ]
 
-        return declarations.free_function_type_t.create_decl_string(
+        return declarations.free_function_type_t(
                 return_type=declarations.void_t()
-                , arguments_types=arguments_types
-                , with_defaults=False)
+                , arguments_types=arguments_types)
     setter_type = property( _get_setter_type )
 
     def _get_has_setter( self ):
         return not declarations.is_const( self.declaration.decl_type )
     has_setter = property( _get_has_setter )
 
-    def _create_impl(self):
-        answer = []
+    def _get_var_type(self):
+        return self.declaration.decl_type
+
+    def _get_var_decl_string(self):
+        return self._get_var_type().decl_string
+
+    def _create_getter(self):
         if self.declaration.type_qualifiers.has_static:
-            substitutions = {
-                'type' : self.declaration.decl_type.decl_string
+            return self.MV_STATIC_GET_TEMPLATE % {
+                'type' : self._get_var_decl_string()
                 , 'name' : self.declaration.name
                 , 'cls_type' : declarations.full_name( self.declaration.parent )
             }
-            answer.append( self.MV_STATIC_GET_TEMPLATE % substitutions)
-            if self.has_setter:
-                answer.append( self.MV_STATIC_SET_TEMPLATE % substitutions )
         else:
-            answer.append( self.MV_GET_TEMPLATE % {
-                'type' : self.declaration.decl_type.decl_string
+            return self.MV_GET_TEMPLATE % {
+                'type' : self._get_var_decl_string()
                 , 'name' : self.declaration.name
-                , 'cls_type' : self.inst_arg_type( has_const=True ) })
-            if self.has_setter:
-                answer.append( self.MV_SET_TEMPLATE % {
-                'type' : self.declaration.decl_type.decl_string
+                , 'cls_type' : self._get_class_inst_type( add_const=True )
+            }
+
+    def _create_setter(self):
+        if self.declaration.type_qualifiers.has_static:
+            return self.MV_STATIC_SET_TEMPLATE % {
+                'type' : self._get_var_decl_string()
                 , 'name' : self.declaration.name
-                , 'cls_type' : self.inst_arg_type( has_const=False ) })
+                , 'cls_type' : declarations.full_name( self.declaration.parent )
+            }
+        else:
+            return self.MV_SET_TEMPLATE % {
+                'type' : self._get_var_decl_string()
+                , 'name' : self.declaration.name
+                , 'cls_type' : self._get_class_inst_type( add_const=False)
+            }
+
+    def _create_impl(self):
+        answer = [self._create_getter()]
+        if self.has_setter:
+            answer.append(self._create_setter())
         return os.linesep.join( answer )
 
     def _get_system_files_impl( self ):
         return []
+
+class mem_var_ref_wrapper_t(member_variable_wrapper_t):
+    """
+    creates get/set accessors for class member variable, that has type reference.
+    """
+
+    def __init__(self, variable ):
+        member_variable_wrapper_t.__init__(self, variable)
+
+    def _get_var_type( self ):
+        type_ = declarations.remove_reference( self.declaration.decl_type )
+        type_ = declarations.remove_const( type_ )
+        if python_traits.is_immutable( type_ ):
+            return type_
+        return self.declaration.decl_type
+
+    def _get_has_setter( self ):
+        no_ref = declarations.remove_reference( self.declaration.decl_type )
+        no_const = declarations.remove_const( no_ref )
+        base_type = declarations.remove_alias( no_const )
+
+        if declarations.is_const(no_ref):
+            return False
+        if declarations.is_enum(base_type):
+            return True
+        if python_traits.is_immutable( self._get_var_type() ):
+            return True
+        if not isinstance( base_type, declarations.declarated_t ):
+            return True #TODO ????
+
+        decl = base_type.declaration
+        if decl.is_abstract:
+            return False
+        if declarations.has_destructor( decl ) and not declarations.has_public_destructor( decl ):
+            return False
+        if not declarations.has_copy_constructor(decl):
+            return False
+        return True
+    has_setter = property( _get_has_setter )
 
 class bit_field_t( member_variable_base_t ):
     """
@@ -512,154 +611,6 @@ class array_mv_wrapper_t( code_creator.code_creator_t
         return [code_repository.array_1.file_name]
 
 
-class mem_var_ref_t( member_variable_base_t ):
-    """
-    creates get/set accessors for class member variable, that has type reference.
-    """
-    def __init__(self, variable, wrapper ):
-        member_variable_base_t.__init__( self, variable=variable, wrapper=wrapper )
-        self.param_sep = os.linesep + self.indent( self.PARAM_SEPARATOR, 2 )
-        self.works_on_instance = False
-
-    def _create_getter( self ):
-        answer = ['def']
-        answer.append( '( ' )
-        answer.append( '"get_%s"' % self.alias)
-        answer.append( self.param_sep )
-        answer.append( '(%s)(&%s)'
-                       % ( self.wrapper.getter_type.decl_string, self.wrapper.getter_full_name ) )
-        if self.declaration.getter_call_policies:
-            if not self.declaration.getter_call_policies.is_default():
-                answer.append( self.param_sep )
-                answer.append( self.declaration.getter_call_policies.create( self ) )
-        else:
-            answer.append( os.linesep + self.indent( '/* undefined call policies */', 2 ) )
-        if self.documentation:
-            answer.append( self.param_sep )
-            answer.append( self.documentation )
-        answer.append( ' )' )
-        return ''.join( answer )
-
-    def _create_setter( self ):
-        answer = ['def']
-        answer.append( '( ' )
-        answer.append( '"set_%s"' % self.alias)
-        answer.append( self.param_sep )
-        answer.append( '(%s)(&%s)'
-                       % ( self.wrapper.setter_type.decl_string, self.wrapper.setter_full_name ) )
-        if self.declaration.setter_call_policies:
-            if not self.declaration.setter_call_policies.is_default():
-                answer.append( self.param_sep )
-                answer.append( self.declaration.setter_call_policies.create( self ) )
-        else:
-            answer.append( os.linesep + self.indent( '/* undefined call policies */', 2 ) )
-        answer.append( ' )' )
-        return ''.join( answer )
-
-    def _create_impl( self ):
-        #TODO: fix me, should not force class scope usage
-        answer = []
-        class_var_name = self.parent.class_var_name
-        answer.append( "%s.%s;" % (class_var_name, self._create_getter() ) )
-        if self.wrapper.has_setter:
-            answer.append( os.linesep )
-            answer.append( "%s.%s;" % (class_var_name, self._create_setter() ) )
-        return ''.join( answer )
-
-class mem_var_ref_wrapper_t( code_creator.code_creator_t
-                             , declaration_based.declaration_based_t ):
-    """
-    creates get/set accessors for class member variable, that has type reference.
-    """
-
-    indent = code_creator.code_creator_t.indent
-    GET_TEMPLATE = os.linesep.join([
-          'static %(type)s get_%(name)s( %(class_type)s& inst ) {'
-        , indent( 'return inst.%(name)s;' )
-        , '}'
-        , ''
-    ])
-
-    SET_TEMPLATE = os.linesep.join([
-          'static void set_%(name)s( %(class_type)s& inst, %(type)s new_value ){ '
-        , indent( 'inst.%(name)s = new_value;' )
-        , '}'
-        , ''
-    ])
-
-    def __init__(self, variable ):
-        code_creator.code_creator_t.__init__( self )
-        declaration_based.declaration_based_t.__init__( self, declaration=variable)
-
-    def _get_getter_full_name(self):
-        return self.parent.full_name + '::' + 'get_' + self.declaration.name
-    getter_full_name = property( _get_getter_full_name )
-
-    def _get_class_inst_type( self ):
-        return declarations.declarated_t( self.declaration.parent )
-
-    def _get_exported_var_type( self ):
-        type_ = declarations.remove_reference( self.declaration.decl_type )
-        type_ = declarations.remove_const( type_ )
-        if python_traits.is_immutable( type_ ):
-            return type_
-        else:
-            return self.declaration.decl_type
-
-    def _get_getter_type(self):
-        return declarations.free_function_type_t(
-                return_type=self._get_exported_var_type()
-                , arguments_types=[ declarations.reference_t( self._get_class_inst_type() ) ] )
-    getter_type = property( _get_getter_type )
-
-    def _get_setter_full_name(self):
-        return self.parent.full_name + '::' + 'set_' + self.declaration.name
-    setter_full_name = property(_get_setter_full_name)
-
-    def _get_setter_type(self):
-        return declarations.free_function_type_t(
-                return_type=declarations.void_t()
-                , arguments_types=[ declarations.reference_t( self._get_class_inst_type() )
-                                    , self._get_exported_var_type() ] )
-    setter_type = property( _get_setter_type )
-
-    def _get_has_setter( self ):
-        if declarations.is_const( declarations.remove_reference( self.declaration.decl_type ) ):
-            return False
-        elif python_traits.is_immutable( self._get_exported_var_type() ):
-            return True
-        else:
-            pass
-
-        no_ref = declarations.remove_reference( self.declaration.decl_type )
-        no_const = declarations.remove_const( no_ref )
-        base_type = declarations.remove_alias( no_const )
-        if not isinstance( base_type, declarations.declarated_t ):
-            return True #TODO ????
-        declaration = base_type.declaration
-        if declaration.is_abstract:
-            return False
-        if declarations.has_destructor( declaration ) and not declarations.has_public_destructor( declaration ):
-            return False
-        if not declarations.has_copy_constructor(declaration):
-            return False
-        return True
-    has_setter = property( _get_has_setter )
-
-    def _create_impl(self):
-        answer = []
-        cls_type = algorithm.create_identifier( self, self.declaration.parent.decl_string )
-
-        substitutions = dict( type=self._get_exported_var_type().decl_string
-                              , class_type=cls_type
-                              , name=self.declaration.name )
-        answer.append( self.GET_TEMPLATE % substitutions )
-        if self.has_setter:
-            answer.append( self.SET_TEMPLATE % substitutions )
-        return os.linesep.join( answer )
-
-    def _get_system_files_impl( self ):
-        return []
 
 class member_variable_addressof_t( member_variable_base_t ):
     """
